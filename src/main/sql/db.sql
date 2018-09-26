13a49d01d03cb59a84ed0afe24db0fdbea719f4d
rafalcl <rafalclab@gmail.com>
2018-09-26 22:07:57 +0200

    Add student table. Modify solution table. Add foreign key to student.

--- src/main/sql/db.sql
+++ src/main/sql/db.sql
@@ -1,4 +1,20 @@
-CREATE TABLE solution(
-  id INT auto_increment primary key,
-  answer TEXT
+-- recreate DB
+DROP DATABASE warsztat3krks04;
+CREATE DATABASE warsztat3krks04;
+USE warsztat3krks04;
+
+-- tables
+CREATE TABLE student (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255)
+);
+
+CREATE TABLE solution (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  submission_date DATETIME NOT NULL,
+  answer TEXT,
+  student_id INT,
+  FOREIGN KEY(student_id) REFERENCES student(id)
+      ON DELETE CASCADE
 );
