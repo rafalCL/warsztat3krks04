43d9312f926de9a5ab253bffecd1c1c53cbcfc70
rafalcl <rafalclab@gmail.com>
2018-09-26 15:57:29 +0200

    Configure DB connection. Add DbUtil

--- src/main/java/pl/coderslab/warsztat3krks04/servlet/HomeServlet.java
+++ src/main/java/pl/coderslab/warsztat3krks04/servlet/HomeServlet.java
@@ -1,17 +1,22 @@
 package pl.coderslab.warsztat3krks04.servlet;
 
+import pl.coderslab.warsztat3krks04.utils.DbUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet(name = "HomeServlet", urlPatterns = "/")
 public class HomeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         getServletContext()
-                .getRequestDispatcher("/index.jsp")
+                .getRequestDispatcher("/home.jsp")
                 .forward(request, response);
     }
 }
